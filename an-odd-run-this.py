d5dba45e70df71d4f04bca0ad79e2762a6e50d8f
Ganesh VR Kumar <ganzse7en@gmail.com>
2014-07-03 21:04:45 +0530

    Basic Functionality

--- an-odd-run-this.py
+++ an-odd-run-this.py
@@ -2,20 +2,30 @@
 	Betting Odds Analysis
 '''
 
-import csv
-
 from modules.CSVHandler import CSVHandlerClass
 
+resultsDict = {'0':'H', '1':'D', '2':'A'}
+betAmount = 10
+		
 
 def main():
 	
-
 	statsFile = CSVHandlerClass('data/csv/S13.csv')
 
-	results, odds = statsFile.returnResultsAndOdds()
-
+	results, oddsList = statsFile.returnResultsAndOdds()
 	statsFile.closeCSVFile()
 
+	netChange = 0
+	for result, odds in zip(results, oddsList):
+		resultWithHighestOdd = odds.index(max(odds))
+
+		if result == resultsDict[str(resultWithHighestOdd)]:
+			netChange = netChange + betAmount * float(max(odds))
+			#print (netChange)
+
+
+
+	print ("Net Change is -- ", netChange)
 
 if __name__ == '__main__':
 	main()
