0242fcf78c26878660aa7ec4030c0af49df8e28f
Ganesh VR Kumar <ganzse7en@gmail.com>
2014-07-05 20:57:11 +0530

    Season additions in calculation

--- an-odd-run-this.py
+++ an-odd-run-this.py
@@ -6,32 +6,59 @@ from modules.CSVHandler import CSVHandlerClass
 
 resultsDict = {'0':'H', '1':'D', '2':'A'}
 betAmount = 10
-		
+
+
+def secondLargest(oddsList):
+    n1, n2 = '0', '0'
+    for x in oddsList:
+        if float(x) >= float(n1):
+            n1, n2 = x, n1
+        elif float(x) > float(n2):
+            n2 = x
+    return n2		
 
 def main():
 	
-	statsFile = CSVHandlerClass('data/csv/S13.csv')
+	filename = ''
+	season = 13
 
-	results, oddsList = statsFile.returnResultsAndOdds()
-	statsFile.closeCSVFile()
+	while (season > 2):
+		if season > 9 :
+			filename = 'data/csv/S' + str(season) + '.csv'
+		else:
+			filename = 'data/csv/S0' + str(season) + '.csv'
 
-	netChangeOne = 0
-	netChangeTwo = 0
+		season = season - 1
 
-	for result, odds in zip(results, oddsList):
-		resultWithHighestOdd = odds.index(max(odds))
-		resultWithLowestOdd = odds.index(min(odds))
+		statsFile = CSVHandlerClass(filename)
 
-		if result == resultsDict[str(resultWithHighestOdd)]:
-			netChangeOne = netChangeOne + betAmount * float(max(odds))
-			#print (max(odds), " - ", netChange)
+		results, oddsList = statsFile.returnResultsAndOdds()
+		statsFile.closeCSVFile()
 
-		elif result == resultsDict[str(resultWithLowestOdd)]:
-			netChangeTwo = netChangeTwo + betAmount * float(min(odds))
-			#print (max(odds), " - ", netChange)
+		netChangeOne = 0
+		netChangeTwo = 0
+		netChangeThree = 0
 
-	print ("Net Change is -- ", netChangeOne)
-	print ("Net Change is -- ", netChangeTwo)
+		for result, odds in zip(results, oddsList):
+			resultWithHighestOdd = odds.index(max(odds))
+			resultWithLowestOdd = odds.index(min(odds))
+			resultWithSecondHighestOdd = odds.index(secondLargest(odds))
+
+			if result == resultsDict[str(resultWithHighestOdd)]:
+				netChangeOne = netChangeOne + betAmount * float(max(odds))
+				#print (max(odds), " - ", netChange)
+
+			elif result == resultsDict[str(resultWithLowestOdd)]:
+				netChangeTwo = netChangeTwo + betAmount * float(min(odds))
+				#print (max(odds), " - ", netChange)
+
+			else:
+				netChangeThree = netChangeThree + betAmount * float(secondLargest(odds))
+
+		print ("\n\nSeason ", season+1)
+		print ("If you bet on the highest odds, Gross revenue -- ", netChangeOne)
+		print ("If you bet on the lowest odds, Gross revenue -- ", netChangeTwo)
+		print ("If you bet on the second largest odds, Gross revenue -- ", netChangeThree)
 
 if __name__ == '__main__':
 	main()
